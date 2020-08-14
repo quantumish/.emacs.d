a90f8eed4989b55143809bd6562b3b3f63124bc0
David Freifeld <freifeld.david@gmail.com>
2020-08-14 11:58:47 -0700

    LSP fixes + hooks

--- init.el
+++ init.el
@@ -502,17 +502,18 @@ function is a convenience wrapper used by `describe-package-1'."
               (:foreground "#cfd1d1")))))
 
 ;; I'm tired of lsp-ui looking terrible
-;(set-face-attribute 'lsp-ui-sideline-code-action nil
-;  :foreground "#999999")
+(lsp-ui-mode 1)
+(set-face-attribute 'lsp-ui-sideline-code-action nil
+  :foreground "#999999")
 (setq lsp-ui-doc-header t)
 (custom-set-faces
    '(lsp-ui-doc-background ((t :background "#fafafa")))
    '(lsp-ui-doc-header ((t :background "#f0f0f0")))
    '(lsp-ui-doc-url ((t :inherit link))))
-(setq lsp-ui-doc-border "orange")
+(setq lsp-ui-doc-border "#999999")
 (setq lsp-ui-doc-position 'bottom)
 (setq lsp-ui-doc-delay 3)
-(setq lsp-ui-sideline-delay 1)3
+(setq lsp-ui-sideline-delay 1)
 
 ;; Some functionality to go with it.
 ;; David Freifeld
@@ -528,6 +529,21 @@ function is a convenience wrapper used by `describe-package-1'."
 
 (global-flycheck-mode)
 (yas-global-mode 1)
+
+(defun code-visuals-hook ()
+  ;; Makes code buffers look nicer
+  (olivetti-mode 1)
+  (olivetti-set-width 100)
+  (focus-mode 1)
+  (visual-line-mode 1))
+(defun code-features-hook ()
+  ;; LSP, flycheck, and co.
+  (lsp)
+  (lsp-ui-mode 1)
+  (flycheck-mode 1)
+  (visual-line-mode 1))
+(add-hook 'c-mode-common-hook 'code-visuals-hook)
+(add-hook 'python-mode-hook 'code-visuals-hook)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 ;; Sequential projects like OmniFocus
