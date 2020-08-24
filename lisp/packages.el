a0122a5ec5d5cd2f2318770cd59ee051617e33bc
David Freifeld <freifeld.david@gmail.com>
2020-08-24 10:19:29 -0700

    add package script

---
+++ lisp/packages.el
@@ -0,0 +1,12 @@
+(require 'package)
+(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
+                          ("gnu" . "http://elpa.gnu.org/packages/")
+                          ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+; Apparently needed for the package auto-complete (why?)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+(setq url-http-attempt-keepalives nil)
+(setq package-check-signature nil)
