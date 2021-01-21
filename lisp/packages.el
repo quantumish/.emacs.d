6ef68d2d82030aa379017fc7e8b5ff720bc4bc3a
David Freifeld <freifeld.david@gmail.com>
2021-01-20 21:47:50 -0800

    Code config and packages

--- lisp/packages.el
+++ lisp/packages.el
@@ -1,11 +1,14 @@
 (require 'package)
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
-                          ("gnu" . "http://elpa.gnu.org/packages/")
-                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-
-(package-initialize)
-(setq url-http-attempt-keepalives nil)
-(setq package-check-signature nil) 
+(defun package-fixer ()
+  (interactive)
+  (setq package-archives '(("ELPA" . "http://tromey.com/elpa/") 
+                           ("gnu" . "http://elpa.gnu.org/packages/")))
+  
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/") t)
+  
+  (package-initialize)
+  (setq url-http-attempt-keepalives nil)
+  (setq package-check-signature nil))   ;
+(package-fixer)
