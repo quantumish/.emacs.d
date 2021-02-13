2decbdb31cc92812f33a620ce4078872ac8d57fe
David Freifeld <freifeld.david@gmail.com>
2021-02-13 11:16:45 -0800

    Org mode config plans

--- org.el
+++ org.el
@@ -1,3 +1,5 @@
+;; I want to be able to complete tasks
+
 (defun org-archive-done-tasks ()
   (interactive)
   (org-map-entries
@@ -13,6 +15,42 @@
 (setq org-archive-location (concat org-directory "/archived.org::"))
 (setq org-archive-file-header-format "")
 
+;; Sequential projects
+
 (setq org-enforce-todo-dependencies t)
 (setq org-enforce-todo-checkbox-dependencies t)
 (setq org-agenda-dim-blocked-tasks t)
+
+;; Recurring todos
+
+(setq org-modules (append org-modules '(org-habit)))
+
+;; Forecast / Agenda
+
+
+;; Calendar
+
+;; Notifications
+
+;; Perspectives
+
+;; Org-capture
+
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                               (file+headline "~/Dropbox/org/inbox.org" "Tasks")
+                               "* TODO %i%?")
+                              ("s" "Schedule" entry
+                               (file+headline "~/Dropbox/org/schedule.org" "Schedule")
+                               "* %i%? \n %U")
+                              ))
+
+(setq org-refile-targets '(("~/Dropbox/org/projects.org" :maxlevel . 3)
+                           ("~/Dropbox/org/schedule.org" :maxlevel . 2)))
+
+;; Tags
+
+;; Project review
+
+;; Focus mode (Only one type of work)
+
+;; Live editing?
